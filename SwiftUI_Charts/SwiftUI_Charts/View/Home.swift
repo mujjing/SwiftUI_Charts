49299c6a38b7a48a388c7dfaea6baef685283454
전지훈 <junji636@gmail.com>
2022-11-19 23:38:41 +0900

    뷰 작업 & 그래프 애니메이션 일부 추가

--- SwiftUI_Charts/SwiftUI_Charts/View/Home.swift
+++ SwiftUI_Charts/SwiftUI_Charts/View/Home.swift
@@ -6,10 +6,40 @@ import SwiftUI
 import Charts
 
 struct Home: View {
+    //MARK: State Chart Data for Animation Changes
+    @State var sampleAnalytics: [SiteView] = sample_analytics
+    @State var currentTab: String = "7 Days"
     var body: some View {
         NavigationStack {
             VStack {
-                AnimatedChart()
+                VStack(alignment: .leading, spacing: 12) {
+                    HStack {
+                        Text("Views")
+                            .fontWeight(.semibold)
+                        
+                        Picker("", selection: $currentTab) {
+                            Text("7 Days")
+                                .tag("7 Days")
+                            Text("Week")
+                                .tag("Week")
+                            Text("Month")
+                                .tag("Month")
+                        }
+                        .pickerStyle(.segmented)
+                    }
+                    
+                    let totalValue = sampleAnalytics.reduce(0.0) { partialResult, item in item.views + partialResult } ?? 0.0
+                    
+                    Text(totalValue.stringFormat)
+                        .font(.largeTitle.bold())
+                    
+                    AnimatedChart()
+                }
+                .padding()
+                .background {
+                    RoundedRectangle(cornerRadius: 10, style: .continuous)
+                        .fill(.white.shadow(.drop(radius: 2)))
+                }
             }
             .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .top)
             .padding()
@@ -19,8 +49,27 @@ struct Home: View {
     
     @ViewBuilder
     func AnimatedChart() -> some View {
+        let max = sampleAnalytics.max { item1, item2 in return item2.views > item1.views }?.views ?? 0
+        
         Chart {
-            
+            ForEach(sampleAnalytics) { item in
+                //MARK: bar graph
+                //MARK: Animation graph
+                BarMark (
+                    x: .value("Hour", item.hour, unit: .hour),
+                    y: .value("Views", item.animate ? item.views : 0)
+                )
+            }
+        }
+        //MARK: Customizing Y-Axis Length
+        .chartYScale(domain: 0...(max + 10000))
+        .frame(height: 250)
+        .onAppear {
+            for (index, _) in sampleAnalytics.enumerated() {
+                withAnimation(.interactiveSpring(response: 0.8, dampingFraction: 0.8, blendDuration: 0.8).delay(Double(index) * 0.05)) {
+                    sampleAnalytics[index].animate = true
+                }
+            }
         }
     }
 }
@@ -28,5 +77,18 @@ struct Home: View {
 struct Home_Previews: PreviewProvider {
     static var previews: some View {
         Home()
+    }
+}
+
+extension Double {
+    var stringFormat: String {
+        if self >= 1000 && self < 999999 {
+            return String(format: "%.1fK", self / 1000).replacingOccurrences(of: ".0", with: "")
+        }
+        if self > 999999 {
+            return String(format: "%.1fM", self / 1000000).replacingOccurrences(of: ".0", with: "")
+        }
+        
+        return String(format: "0.f", self)
     }
 }
